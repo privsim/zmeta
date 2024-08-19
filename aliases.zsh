bf672e62f1ac9e61d366fb11f977c27ab9782774
privsim <excaliberswake@pm.me>
2024-08-18 21:49:39 -0600

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,7 @@ alias ea="cat /proc/sys/kernel/random/entropy_avail"
 alias 64AN='openssl rand -base64 48 | tr -d '+/=' | cut -c -64 | pbcopy'
 alias 64ANU='tr -dc 'A-Za-z0-9' < /dev/urandom | head -c64 | pbcopy'
 alias hashpass='python ~/.zmeta/bin/argon2_hashgen.py'
+alias 32AN='openssl rand -base64 32 | tr -dc 'a-zA-Z0-9!@#$%^&*()' | head -c 32 | pbcopy'
 
 # mask built-ins with better defaults
 alias cp='cp -i'
